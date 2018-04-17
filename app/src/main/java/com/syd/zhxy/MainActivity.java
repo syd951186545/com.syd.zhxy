ffb3db0620807ab6d6cbb3a5544f52cbb040a58e
syd951186545 <951186545@qq.com>
2018-04-17 19:02:53 +0800

    repair mainActivity

--- app/src/main/java/com/syd/zhxy/MainActivity.java
+++ app/src/main/java/com/syd/zhxy/MainActivity.java
@@ -6,43 +6,56 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DBManager myDBManager ;
+    private MyDBHelper helper ;
     private EditText account;   //学号
     private EditText password;   //密码
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDBManager = new DBManager(this);
-
+        helper= new MyDBHelper(this);
 
         //点击注册按钮进入注册页面
-    public void logonClicked(View view){
-        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
-        startActivity(intent);
-    }
-        //点击登录按钮
-    public void loginClicked(View view) {
-        account=(EditText)findViewById(R.id.editText2);
-        password=(EditText)findViewById(R.id.editText);
-        String userName=account.getText().toString();
-        String passWord=password.getText().toString();
-        if (login(userName,passWord)) {
-            Toast.makeText(MainActivity.this, "登陆成功（ZY，111）", Toast.LENGTH_SHORT).show();
-         }
-        else {
-            Toast.makeText(MainActivity.this, "登陆失败", Toast.LENGTH_SHORT).show();
-         }
-    }
+        Button bt_signin = (Button) findViewById(R.id.bt_signin);
+        bt_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
 
+        //点击登录按钮
+        Button bt_login = (Button) findViewById(R.id.bt_login);
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                account=(EditText)findViewById(R.id.editText2);
+                password=(EditText)findViewById(R.id.editText);
+                String userName=account.getText().toString();
+                String passWord=password.getText().toString();
+                if (check_login(userName,passWord)) {
+                Toast.makeText(MainActivity.this, "登陆成功（ZY，111）", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "登陆失败", Toast.LENGTH_SHORT).show();
+                }
+        }
+
+    });
+
+
+    }
     //验证登录
-    public boolean login(String username,String password) {
-        SQLiteDatabase db = myDBManager.getWritableDatabase();
+    private boolean check_login(String username, String password) {
+        SQLiteDatabase db = helper.getWritableDatabase();
         String sql = "select * from userData where name=? and password=?";
         Cursor cursor = db.rawQuery(sql, new String[] {username, password});
         if (cursor.moveToFirst()) {
@@ -50,9 +63,5 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
-
-
-
     }
 }
--- app/src/main/java/com/syd/zhxy/MyDBHelper.java
+++ app/src/main/java/com/syd/zhxy/MyDBHelper.java
@@ -17,25 +17,52 @@ public class MyDBHelper extends SQLiteOpenHelper{
     private static final String DATABASE_NAME = "ZHXYDB.db";
     //数据库的版本号
     private static final int DATABASE_VERSION = 1;
-    //数据库管理者
-    private DBManager DBManager;
-
+    //数据库表单
+    private final String login_table = "login_user" ;
+    private final String school_allpeople_table = "school_allpeople" ;
 
 
     public MyDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        DBManager.initDB(db);
+        //第一次调用数据库时初始化,生成1.学校人员信息数据表,2.注册用户存储表
+        String sql1 = "create table if not exists ["+login_table+"] "+"("+"ID"+" integer primary key , "
+                +"name"+"varchar, "
+                +"password"+" integer"
+                +"authentication"+"varchar"+")";
+        String sql2 = "create table if not exists ["+school_allpeople_table+"] "+"("+"ID"+" integer primary key , "
+                +"name"+"varchar, "
+                +"authentication"+" varchar)";
+        Log.i("info","create table = "+login_table) ;
+        Log.i("info","create table = "+school_allpeople_table) ;
+        db.execSQL(sql1);
+        db.execSQL(sql2);
+        //初始化school表中人员信息
+        ArrayList<Person> persons = new ArrayList<>();
+
+        Person person1 = new Person(20175301, "孙玉东", "学生");
+        Person person2 = new Person(20175302, "于尤婧", "学生");
+        Person person3 = new Person(00000001, "程老师", "老师");
+
+        db.beginTransaction();  //开始事务
+        try {
+            for (Person personx : persons) {
+                db.execSQL("INSERT INTO "+school_allpeople_table+" VALUES(?, ?, ?)", new Object[]{personx.Id, personx.name, personx.authentication});
+            }
+            db.setTransactionSuccessful();  //设置事务成功完成
+        } finally {
+            db.endTransaction();    //结束事务
+        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //更新数据库版本
         //TODO
+
 
     }
 }
